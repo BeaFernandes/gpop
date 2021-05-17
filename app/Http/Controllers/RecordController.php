51dc822bf5d70d734303a401d88639d6456ab083
guilhermeferreira7 <guilhermefribas@outlook.com>
2021-05-17 17:33:48 -0300

    update

--- app/Http/Controllers/RecordController.php
+++ app/Http/Controllers/RecordController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Record;
 use Illuminate\Http\Request;
 
-class ReportController extends Controller
+class RecordController extends Controller
 {
     /**
      * Display a listing of the resource.
