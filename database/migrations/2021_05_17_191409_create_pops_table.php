48253c8bee375342e69018b7dc8c2cb5197b7c31
guilhermeferreira7 <guilhermefribas@outlook.com>
2021-05-17 17:10:05 -0300

    Adicionados classes de modelos e migrations

---
+++ database/migrations/2021_05_17_191409_create_pops_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePopsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pops', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 30);
+            $table->string('key_word', 100);
+            $table->string('description', 2000);
+            $table->string('image', 30);
+            $table->foreignId('categories_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pops');
+    }
+}
