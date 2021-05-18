08c007fc2f9d7b3f22986fc6754e1b9c60b5c374
guilhermeferreira7 <guilhermefribas@outlook.com>
2021-05-18 01:19:30 -0300

    Revert "mudei algo?"
    This reverts commit cfa153d04f7b89fe5aed9d5fd34774feeb6164bd.

--- resources/views/pop/create.blade.php
+++ resources/views/pop/create.blade.php
@@ -5,37 +5,35 @@
     <div class="container">
         <div class="row justify-content-md-center">
             <div class="col col-sm-12 col-lg-8">
-                <form>
+                <form action="" method="POST">
                     <div class="mb-3">
                         <label for="title" class="form-label">Título*</label>
-                        <input type="text" class="form-control" id="title">
+                        <input type="text" class="form-control" id="title" name="title" required>
                     </div>
-                    <label for="class" class="form-label">Categoria*</label>
-                    <select id="class" class="form-select mb-3">
+                    <label for="category" class="form-label">Categoria*</label>
+                    <select id="category" class="form-select mb-3" name="category" required>
                         <option selected>----</option>
-                        <option value="1">One</option>
-                        <option value="2">Two</option>
-                        <option value="3">Three</option>
+                        <option value="1">exemplo</option>
+                        <option value="2">exemplo</option>
+                        <option value="3">exemplo</option>
                     </select>
                     <div class="mb-3">
                         <label for="description" class="form-label">Descrição*</label>
-                        <input type="text" class="form-control" id="description">
+                        <textarea class="form-control" id="description" rows="3" name="description" required></textarea>
+                    </div>
+                    <div class="input-group mb-3">
+                        <input type="file" class="form-control" id="img">
                     </div>
                     <div class="mb-3">
-                        <div class="row">
-                            <div class="col">Passo a passo</div>
-                            <div class="col text-end" id="plus"><i class="fa fa-plus text-highlight-1 fs-6" aria-hidden="true"></i></div>
-                        </div>
+                        <label for="key-word" class="form-label">Palavras chaves</label>
+                        <input type="text" class="form-control text-text-bg-3" id="key-word" name="key-word">
                     </div>
-                    <div class="mb-3 visually-hidden" id="steps">
-                        <label for="step" class="form-label">Passo 1</label>
-                        <input type="text" class="form-control" id="step">
+                    <div class="mb-3 text-end">
+                        <spam class="text-text-bg-3 fs-6">Campos com * são obrigatórios</spam>
                     </div>
-                    <div class="mb-3 form-check">
-                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                    <div class="text-end">
+                        <button type="submit" class="btn btn-highlight-2 text-text-bg-2 fw-bold">Salvar</button>
                     </div>
-                    <button type="submit" class="btn btn-primary">Submit</button>
                 </form>
             </div>
         </div>
@@ -44,14 +42,6 @@
 
     <script src="{{ asset('site/jquery.js') }}"></script>
     <script src="{{ asset('site/bootstrap.js') }}"></script>
-
-
-    <script>
-        $('#plus').click(function () {
-            console.log("alos");
-            $( "#steps" ).show();
-        })
-    </script>
 </body>
 
 </html>
