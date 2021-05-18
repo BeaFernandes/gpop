9ffa3df3df4400c2445c64e644bc30cae02ad3c9
guilhermeferreira7 <guilhermefribas@outlook.com>
2021-05-18 01:05:50 -0300

    acho que deu

--- routes/web.php
+++ routes/web.php
@@ -17,13 +17,30 @@ Route::get('/', function () {
     return view('dashboard', ['title' => 'Dashboard']);
 });
 
+Route::get('/login', function () {
+    return view('login.create', ['title' => 'Login']);
+});
+
 Route::post('/pop', function () {
     return view('save');
 });
 Route::get('/pop.create', function () {
     return view('pop/create', ['title' => 'Novo POP']);
 });
+Route::get('/pop.edit', function () {
+    return view('pop/edit', ['title' => 'Alterar POP']);
+});
 
+
+Route::get('/user', function () {
+    return view('user.index', ['title' => 'Usuários']);
+});
+Route::get('/user.create', function () {
+    return view('user/create', ['title' => 'Novo Usuário']);
+});
+Route::get('/user.edit', function () {
+    return view('user/edit', ['title' => 'Alterar Usuário']);
+});
 
 
 
